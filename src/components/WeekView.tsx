2bf6e2959b51145d80d2a3e4a2d9363267a57ccb
Matt Gehrls <mattgehrls@gmail.com>
2025-01-02 11:42:16 -0500

    removed black background

--- src/components/WeekView.tsx
+++ src/components/WeekView.tsx
@@ -10,7 +10,7 @@ const WeekView = ({
   currentDate: Date;
 }) => {
   return (
-    <div className="flex h-full flex-col gap-[1px] bg-black p-[1px]">
+    <div className="flex h-full flex-col overflow-hidden rounded-3xl bg-white">
       <Weekdays />
       <div className="grid h-full grid-cols-7 grid-rows-[auto] gap-[1px] overflow-auto">
         {weekDatesToRender.map((date, index) => {
