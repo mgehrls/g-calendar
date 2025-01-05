ebec83d6621a155ea459f1eb774a81e9f5e235e8
Matt Gehrls <mattgehrls@gmail.com>
2025-01-05 13:51:21 -0500

    deemphasized days outside of the month

--- src/components/MonthView.tsx
+++ src/components/MonthView.tsx
@@ -16,15 +16,22 @@ const MonthView = ({
     <div className="flex h-full flex-col p-[1px] bg-gray-300 rounded-3xl">
       <div className="grid h-full grid-cols-7 grid-rows-[auto] overflow-auto gap-[1px] rounded-3xl">
         {monthDatesToRender.map((date, index) => {
+          const notCurrentMonth =
+            (date > 20 && index < 7) || (date < 10 && index > 20);
           return (
             <div
               key={index}
-              className={clsx("py-2 flex flex-col items-center bg-white text-xs", 
-                currentDateOnDisplay && date === currentDate.getDate() ? "bg-blue-300" : "", 
-                index === 0 && 'rounded-tl-3xl',
-                index === 6 && 'rounded-tr-3xl',
-                index === bottomLeft && 'rounded-bl-3xl',
-                index === bottomRight && 'rounded-br-3xl')}
+              className={clsx(
+                "flex flex-col items-center bg-white py-2 text-xs",
+                currentDateOnDisplay && date === currentDate.getDate()
+                  ? "bg-blue-300"
+                  : "",
+                notCurrentMonth && "bg-gray-100 text-gray-500",
+                index === 0 && "rounded-tl-3xl",
+                index === 6 && "rounded-tr-3xl",
+                index === bottomLeft && "rounded-bl-3xl",
+                index === bottomRight && "rounded-br-3xl",
+              )}
             >
               {index < 7 && <p>{getDaysShort(index)}</p>}
               <p>{date}</p>
