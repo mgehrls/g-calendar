7e3798e877af99a4f85f522339b0c5a3ff61f5d3
Matt Gehrls <mattgehrls@gmail.com>
2025-01-06 13:33:22 -0500

    added reusable function from dayview

--- src/components/WeekView.tsx
+++ src/components/WeekView.tsx
@@ -4,6 +4,7 @@ import WeekDates from "./WeekDates";
 import Weekdays from "./Weekdays";
 import { filterDaysEvents, type Event } from "~/utils/fakeEvents";
 import clsx from "clsx";
+import { renderDaysEvents } from "~/utils/renderDates";
 
 const WeekView = ({
   events,
@@ -83,33 +84,7 @@ const WeekView = ({
                       <div className="absolute h-[1px] w-full bg-gray-300" />
                     </div>
                   ))}
-                  {daysEvents.map((event, i) => {
-                    const startHour = event.dateStart.getHours();
-                    const startMinute = event.dateStart.getMinutes();
-                    const endHour = event.dateEnd.getHours();
-                    const endMinute = event.dateEnd.getMinutes();
-                    const top = startHour * 40 + (startMinute / 60) * 40;
-                    const height =
-                      (endHour - startHour) * 40 +
-                      ((endMinute - startMinute) / 60) * 40;
-
-                    console.log(event.title, top, height);
-                    return (
-                      <div
-                        key={event.id}
-                        className={`absolute rounded-md border-[1px] border-blue-800 bg-blue-500 px-2 text-white`}
-                        style={{
-                          left: `${i * 15}%`,
-                          top: `${top}px`,
-                          height: `${height}px`,
-                          width: `${85 - 15 * i}%`,
-                          zIndex: index + 1,
-                        }}
-                      >
-                        <p className="truncate">{event.title}</p>
-                      </div>
-                    );
-                  })}
+                  {renderDaysEvents(daysEvents)}
                 </div>
               </div>
             );
